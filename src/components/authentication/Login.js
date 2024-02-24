9546767c7cfce914562052185ad29e20e54c5217
Ishu Mehta <mehta.ishu14@gmail.com>
2024-02-24 19:25:39 +0530

    fixed proxy for vercel

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -41,7 +41,7 @@ const Login = () => {
             };
 
             const { data } = await axios.post(
-                "/api/user/login",
+                "https://task-manager-backend-production-81bc.up.railway.app/api/user/login",
                 { email, password },
                 config
             );
